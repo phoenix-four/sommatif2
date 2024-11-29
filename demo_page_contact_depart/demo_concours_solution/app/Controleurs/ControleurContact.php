cac4126a1498389db73ecd6039886e4b1c27c41a
phoenix-four <119964971+phoenix-four@users.noreply.github.com>
2024-11-29 10:26:58 -0500

    race againts the machine

--- demo_page_contact_depart/demo_concours_solution/app/Controleurs/ControleurContact.php
+++ demo_page_contact_depart/demo_concours_solution/app/Controleurs/ControleurContact.php
@@ -36,10 +36,24 @@ class ControleurContact
 
     public function inserer(): void
     {
-        $fichierJson = file_get_contents("../ressources/lang/fr_CA.UTF-8/messagesValidation.json"); // Récupérer le contenu des messages en format JSON
-        $tMessagesJson = json_decode($fichierJson, true); // Convertir en tableau associatif
+
 
         // Valider chaque champ et garder le résultat dans tValidation
+
+        $tValidation = [];
+        $tValidation = $this->validation();
+
+
+        var_dump($tValidation);
+
+        // À compléter...
+    }
+
+
+    private function validation(): array
+    {
+        $fichierJson = file_get_contents("../ressources/lang/fr_CA.UTF-8/messagesValidation.json"); // Récupérer le contenu des messages en format JSON
+        $tMessagesJson = json_decode($fichierJson, true); // Convertir en tableau associatif
         $tValidation = [];
         $tValidation = Validateur::validerChamp('nom', "#^[a-zA-ZÀ-ÿ' -]+$#", $tMessagesJson, $tValidation, true);
         $tValidation = Validateur::validerChamp('courriel', "#^[a-zA-Z0-9][a-zA-Z0-9-]+([.][a-zA-Z0-9-]+)[@][a-zA-Z0-9-]+([.][a-zA-Z0-9-]+)[.][a-zA-Z]{2,}$#", $tMessagesJson, $tValidation, true);
@@ -49,11 +63,6 @@ class ControleurContact
         $tValidation = Validateur::validerChamp('message', "#^[ -.0-9a-zA-ZÀ-ÿ';!?éèàùâêîôûäëïöüoeçÇ'«»=@:]*$#", $tMessagesJson, $tValidation, true);
         $tValidation = Validateur::validerChamp('responsable_id', "#^\d{1}$#", $tMessagesJson, $tValidation, true);
 
-        var_dump($tValidation);
-
-        // À compléter...
+        return $tValidation;
     }
-
-
-    private function validation() {}
 }
