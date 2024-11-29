8dc4f0768c991e83f8f0324a3428bcdc0c1c549e
phoenix-four <119964971+phoenix-four@users.noreply.github.com>
2024-11-29 10:06:53 -0500

    chapala pa

--- demo_page_contact_depart/demo_concours_solution/app/Controleurs/ControleurContact.php
+++ demo_page_contact_depart/demo_concours_solution/app/Controleurs/ControleurContact.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controleurs;
@@ -11,22 +12,25 @@ use App\App;
 class ControleurContact
 {
 
-    public function __construct(){
-    }
+    public function __construct() {}
 
     public function creer(): void
     {
-       
+
         $tValidation = null;
 
-         // Récupérer tValidation de la session, ou null si non défini
-        
-         // À compléter...
- 
+        if (isset($_SESSION['tValidation'])) {
+            $tValidation = $_SESSION['tValidation'];
+        }
+        // Récupérer tValidation de la session, ou null si non défini
+
+        // À compléter...
+
+
 
         // Assembler la vue
         $tDonnees = array("tValidation" => $tValidation);
-        echo App::getBlade()->run("contacts.creer",$tDonnees);
+        echo App::getBlade()->run("contacts.creer", $tDonnees);
     }
 
 
@@ -37,11 +41,16 @@ class ControleurContact
 
         // Valider chaque champ et garder le résultat dans tValidation
         $tValidation = [];
-        $tValidation = Validateur::validerChamp('nom',"#^[a-zA-ZÀ-ÿ' -]+$#", $tMessagesJson, $tValidation, true);
-        
+        $tValidation = Validateur::validerChamp('nom', "#^[a-zA-ZÀ-ÿ' -]+$#", $tMessagesJson, $tValidation, true);
+        $tValidation = Validateur::validerChamp('courriel', "^[a-zA-Z0-9][a-zA-Z0-9-]+([.][a-zA-Z0-9-]+)[@][a-zA-Z0-9-]+([.][a-zA-Z0-9-]+)[.][a-zA-Z]{2,}$", $tMessagesJson, $tValidation, true);
+        $tValidation = Validateur::validerChamp('telephone', "^\d{10}$", $tMessagesJson, $tValidation, true);
+        $tValidation = Validateur::validerChamp('acceptation_partage', "^(on|off)$", $tMessagesJson, $tValidation, true);
+        $tValidation = Validateur::validerChamp('sujet', "#^[ -.0-9a-zA-ZÀ-ÿ';!?éèàùâêîôûäëïöüoeçÇ'«»=@:]*$#", $tMessagesJson, $tValidation, true);
+        $tValidation = Validateur::validerChamp('message', "#^[ -.0-9a-zA-ZÀ-ÿ';!?éèàùâêîôûäëïöüoeçÇ'«»=@:]*$#", $tMessagesJson, $tValidation, true);
+        $tValidation = Validateur::validerChamp('responsable_id', "^\d{1}$", $tMessagesJson, $tValidation, true);
+
         var_dump($tValidation);
 
         // À compléter...
     }
 }
-
